78c9ae69a6731497abffa578fdaf677caf9777c9
Anjani <anjanison9@gmail.com>
2023-06-29 10:24:25 +0530

    Set up done

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const colors = require('colors')
+const dotenv = require('dotenv')
+const morgan = require('morgan')
+const cors = require('cors')
+
+// config dotenv
+dotenv.config()
+
+// rest objects
+const app = express()
+
+// middlewares
+app.use(express.json())
+app.use(cors())
+app.use(morgan('dev'))
+
+// routes
+app.get('/', (req, res) => {
+    res.send('Hello World! from server')
+}
+)
+// server
+const PORT = process.env.PORT || 8080
+
+// listening to server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`.yellow.bold)
+})
+
+
+
+// package.json
